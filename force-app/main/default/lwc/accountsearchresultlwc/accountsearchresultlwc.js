ae9de6a6910287195ab3500161dd651786d11544
Shubham <Shubham@DESKTOP-RR5JPF9>
2021-09-01 12:20:56 +0530

    Added new column to datatable

--- force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
+++ force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
@@ -3,7 +3,8 @@ import { LightningElement, api } from 'lwc';
 export default class Accountsearchresultlwc extends LightningElement {
     columns = [
         { label: 'Account Name', fieldName: 'accountName' },
-        { label: 'Phone', fieldName: 'accountPhone' }
+        { label: 'Phone', fieldName: 'accountPhone' },
+        { label: 'Billing Address', fieldName: 'billingAddress' }
     ];
     @api searchResults;
 }
