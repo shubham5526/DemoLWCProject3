fa2c6036d458d0f19240f84e6aabd0652cb6d96f
Shubham <Shubham@DESKTOP-RR5JPF9>
2021-09-02 11:06:48 +0530

    Subscribe and Unsubscribe LMChannel

--- force-app/main/default/lwc/accountdetailslwc/accountdetailslwc.js
+++ force-app/main/default/lwc/accountdetailslwc/accountdetailslwc.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import accountDetailsMC1 from '@salesforce/messageChannel/Account_Details__c';
+import { subscribe, MessageContext, unsubscribe } from 'lightning/messageService';
 
-export default class Accountdetailslwc extends LightningElement {}
+export default class Accountdetailslwc extends LightningElement {
+    dataFromMessageChannel;
+    messageSubscriber;
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback() {
+        this.messageSubscriber = subscribe(this.messageContext, accountDetailsMC1, (msg) => this.getAccountDetails(msg));
+    }
+
+    disconnectedCallback() {
+        unsubscribe(this.messageSubscriber);
+    }
+
+    getAccountDetails(message) {
+        console.log(message);
+        this.dataFromMessageChannel = message.accountDetails;
+    }
+
+}
