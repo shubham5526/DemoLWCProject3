7c6bc72c94605c76ee4a8744d8e58eef0061299a
Shubham <Shubham@DESKTOP-RR5JPF9>
2021-09-02 09:11:00 +0530

    Adding More Columns to Datatable

--- force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
+++ force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
@@ -2,9 +2,11 @@ import { LightningElement, api } from 'lwc';
 
 export default class Accountsearchresultlwc extends LightningElement {
     columns = [
-        { label: 'Account Name', fieldName: 'accountName' },
-        { label: 'Phone', fieldName: 'accountPhone' },
-        { label: 'Billing Address', fieldName: 'billingAddress' }
+        { label: 'Account Name', fieldName: 'accountName', type: 'text' },
+        { label: 'Phone', fieldName: 'accountPhone', type: 'phone' },
+        { label: 'Website', fieldName: 'website', type: 'url' },
+        { label: 'Billing Address', fieldName: 'billingAddress' },
+        { label: 'Annual Revenue', fieldName: 'annualrevenue', type: 'currency' }
     ];
     @api searchResults;
 }
--- force-app/main/default/lwc/createnewaccountlwc/createnewaccountlwc.js
+++ force-app/main/default/lwc/createnewaccountlwc/createnewaccountlwc.js
@@ -11,7 +11,15 @@ export default class Createnewaccountlwc extends LightningElement {
             event.detail.forEach(x => {
                 var data = {
                     recordId: x.Id,
-                    accountName: x.Name
+                    accountName: x.Name,
+                    accountPhone: x.Phone,
+                    website: x.Website,
+                    annualrevenue: x.AnnualRevenue,
+                    billingAddress: (x.BillingAddress.street == undefined ? '' : x.BillingAddress.street) + ' ' +
+                        (x.BillingAddress.city == undefined ? '' : x.BillingAddress.city) + ' ' +
+                        (x.BillingAddress.state == undefined ? '' : x.BillingAddress.state) + ' ' +
+                        (x.BillingAddress.postalCode == undefined ? '' : x.BillingAddress.postalCode) + ' ' +
+                        (x.BillingAddress.country == undefined ? '' : x.BillingAddress.country)
                 }
                 this.searchResultsParent.push(data);
             });
