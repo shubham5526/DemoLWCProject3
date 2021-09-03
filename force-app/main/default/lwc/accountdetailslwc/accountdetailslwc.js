e2fcfa6e257c021ee418aa38e68238d4a80a8793
Shubham <Shubham@DESKTOP-RR5JPF9>
2021-09-03 12:03:41 +0530

    Exposing a property to App Builder

--- force-app/main/default/lwc/accountdetailslwc/accountdetailslwc.js
+++ force-app/main/default/lwc/accountdetailslwc/accountdetailslwc.js
@@ -1,10 +1,17 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import accountDetailsMC1 from '@salesforce/messageChannel/Account_Details__c';
 import { subscribe, MessageContext, unsubscribe } from 'lightning/messageService';
 
 export default class Accountdetailslwc extends LightningElement {
     dataFromMessageChannel;
     messageSubscriber;
+    @api isVisible;
+    @api year;
+
+    constructor() {
+        super();
+        this.isVisible = true;
+    }
 
     @wire(MessageContext)
     messageContext;
