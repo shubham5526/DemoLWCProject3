cbaf6e22be054343e2ec68f50d1f2f0e3961fdf0
Shubham <Shubham@DESKTOP-RR5JPF9>
2021-09-04 13:34:27 +0530

    getRecord - uiRecordAPI, wire, reactive properties

--- force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
+++ force-app/main/default/lwc/accountsearchresultlwc/accountsearchresultlwc.js
@@ -1,6 +1,10 @@
-import { LightningElement, api } from 'lwc';
-import { deleteRecord } from 'lightning/uiRecordApi';
+import { LightningElement, api, wire } from 'lwc';
+import { deleteRecord, getRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import OWNER_NAME_FIELD from '@salesforce/schema/Account.Owner.Name';
+import PHONE_FIELD from '@salesforce/schema/Account.Phone';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 
 const actions = [
     { label: 'Delete Record', name: 'Delete_Record' },
@@ -16,36 +20,53 @@ export default class Accountsearchresultlwc extends LightningElement {
         { label: 'Action', type: 'action', typeAttributes: { rowActions: actions } }
     ];
     @api searchResults;
+    accRecordId;
+
+    @wire(getRecord, { recordId: '$accRecordId', fields: [NAME_FIELD, INDUSTRY_FIELD], optionalFields: [PHONE_FIELD, OWNER_NAME_FIELD] })
+    accountData({ error, data }) {
+        if (data) {
+            console.log(data);
+        } else if (error) {
+            console.log(error);
+        }
+    }
 
     handleRowAction(event) {
         console.log(event);
         const actioName = event.detail.action.name;
         if (actioName === 'Delete_Record') {
-            deleteRecord(event.detail.row.recordId)
-                .then(x => {
-                    this.searchResults = this.searchResults.filter(function(value, index, arr) {
-                        return value.recordId != event.detail.row.recordId;
-                    });
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Success',
-                            message: 'Account Deleted Succesfully',
-                            variant: 'success',
-                            mode: 'sticky'
-                        })
-                    );
-                })
-                .catch(e => {
-                    console.log(e);
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Error',
-                            message: 'Error in record deletion. ' + e,
-                            variant: 'error',
-                            mode: 'sticky'
-                        })
-                    );
-                })
+            this.deleteRecordMethod(event);
+        } else if (actioName === 'Show_Record') {
+            this.accRecordId = event.detail.row.recordId;
+            console.log(this.accountData);
         }
+    }
+
+    deleteRecordMethod(event) {
+        deleteRecord(event.detail.row.recordId)
+            .then(x => {
+                this.searchResults = this.searchResults.filter(function(value, index, arr) {
+                    return value.recordId != event.detail.row.recordId;
+                });
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account Deleted Succesfully',
+                        variant: 'success',
+                        mode: 'sticky'
+                    })
+                );
+            })
+            .catch(e => {
+                console.log(e);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: 'Error in record deletion. ' + e,
+                        variant: 'error',
+                        mode: 'sticky'
+                    })
+                );
+            })
     }
 }
